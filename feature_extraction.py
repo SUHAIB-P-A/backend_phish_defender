fb5d622a263497e6f2a047b25a9c2e691a8b6fa0
thegreatcode1 <suhaibpa72@gmail.com>
2024-04-09 00:04:02 +0530

    convert the url into standard format and create a dataset list on the starting of the function

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,10 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 import whois
+import re
 
 # Dataset generation function
 def generate_dataset(url):
-    url = "http://" + url
+
+# initialize the dataset list
+    dataset = [0] * 30
+
+
+
+# convert the url in to standard format    
+    if not re.match(r"https?",url):
+       url = "http://" + url
 
 # get and store the response of the inputed url
     try:
