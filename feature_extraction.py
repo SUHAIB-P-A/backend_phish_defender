82b78b17175b05146c8cda23a069e639ccaf6937
thegreatcode1 <suhaibpa72@gmail.com>
2024-04-08 23:48:24 +0530

    request the response of the url

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,4 +1,17 @@
-# Dataset generation function
+import requests
+from bs4 import BeautifulSoup
 
+# Dataset generation function
 def generate_dataset(url):
     url = "http://" + url
+
+# get and store the response of the inputed url
+    try:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text,'html.parser')
+        #print(soup)
+    except:
+        response = ""
+        soup = -999
+        #print(soup)
+
