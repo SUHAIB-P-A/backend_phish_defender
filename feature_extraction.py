8eb0335ca22e0c6f6f89f81940eec3892c61c288
thegreatcode1 <suhaibpa72@gmail.com>
2024-04-11 22:08:13 +0530

    create dummy functions of the url feutures

--- feature_extraction.py
+++ feature_extraction.py
@@ -11,6 +11,119 @@ def having_ip_address(domain):
 def url_length(url):
     print("")
 
+
+def shortening_service(url):
+    print("")
+
+
+def at_in_url(url):
+    print("")
+
+
+def double_slash_redirecting(url):
+    print("")
+
+
+def prefix_suffix(domain):
+    print("")
+
+
+def having_sub_domain(domain):
+    print("")
+
+
+def ssl_final_state(url):
+    print("")
+
+
+def domain_registration_length(whois_response):
+    print("")
+
+
+def favicon(url):
+    print("")
+
+
+def port(domain):
+    print("")
+
+
+def https_token(domain):
+    print("")
+
+
+def request_url(url, domain, soup):
+    print("")
+
+
+def url_of_anchor(url, domain, soup):
+    print("")
+
+
+def links_in_tags(url, domain, soup):
+    print("")
+
+
+def sfh(url):
+    print("")
+
+
+def check_submit_to_email(response):
+    print("")
+
+
+def abnormal_url(url):
+    print("")
+
+
+def web_forwarding(response):
+    print("")
+
+
+def on_mouseover(response):
+    print("")
+
+
+def right_click(response):
+    print("")
+
+
+def popup_window(response):
+    print("")
+
+
+def iframe(response):
+    print("")
+
+
+def age_of_domain(whois_response):
+    print("")
+
+
+def check_dns_record(url):
+    print("")
+
+
+def website_traffic(url):
+    print("")
+
+
+def page_rank(domain):
+    print("")
+
+
+def google_index(url):
+    print("")
+
+
+def links_pointing_to_page(response):
+    print("")
+
+
+def statistical_report(domain):
+    print("")
+
+    
 # Dataset generation function
 def generate_dataset(url):
 
@@ -51,34 +164,34 @@ def generate_dataset(url):
 
     dataset[0] = having_ip_address(domain)
     dataset[1] = url_length(url)
-    dataset[2] =
-    dataset[3] =
-    dataset[4] =
-    dataset[5] =
-    dataset[6] =
-    dataset[7] =
-    dataset[8] =
-    dataset[9] =
-    dataset[10] =
-    dataset[11] =
-    dataset[12] =
-    dataset[13] =
-    dataset[14] =
-    dataset[15] =
-    dataset[16] =
-    dataset[17] =
-    dataset[18] =
-    dataset[19] =
-    dataset[20] =
-    dataset[21] =
-    dataset[22] =
-    dataset[23] =
-    dataset[24] =
-    dataset[25] =
-    dataset[26] =
-    dataset[27] =
-    dataset[28] =
-    dataset[29] =
+    dataset[2] = shortening_service(domain)
+    dataset[3] = at_in_url(url)
+    dataset[4] = double_slash_redirecting(url)
+    dataset[5] = prefix_suffix(domain)
+    dataset[6] = having_sub_domain(domain)
+    dataset[7] = ssl_final_state(url)
+    dataset[8] = domain_registration_length(whois_respo)
+    dataset[9] = favicon(url)
+    dataset[10] = port(domain)
+    dataset[11] = https_token(domain)
+    dataset[12] = request_url(url,domain,soup)
+    dataset[13] = url_of_anchor(url,domain,soup)
+    dataset[14] = links_in_tags(url,domain,soup)
+    dataset[15] = sfh(url)
+    dataset[16] = check_submit_to_email(response)
+    dataset[17] = abnormal_url(url)
+    dataset[18] = web_forwarding(response)
+    dataset[19] = on_mouseover(response)
+    dataset[20] = right_click(response)
+    dataset[21] = popup_window(response)
+    dataset[22] = iframe(response)
+    dataset[23] = age_of_domain(whois_respo)
+    dataset[24] = check_dns_record(url)
+    dataset[25] = website_traffic(url)
+    dataset[26] = page_rank(domain)
+    dataset[27] = google_index(url)
+    dataset[28] = links_pointing_to_page(response)
+    dataset[29] = statistical_report(domain)
 
 # calculate the ending time of dataset creation
     end = time.time()
