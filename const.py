2ac2d0609b4c4e3fdd7e6b35bb02a4573309bdff
thegreatcode1 <suhaibpa72@gmail.com>
2024-04-13 12:36:05 +0530

    reduce the code and create a py file for const items

---
+++ const.py
@@ -0,0 +1,11 @@
+
+
+class constant:
+    famous_short_urls = ["bit.ly", "tinyurl.com", "goo.gl", "rebrand.ly", "t.co", "youtu.be", "ow.ly", "w.wiki", "is.gd",
+                         "shorte.st", "go2l.ink", "x.co", "tr.im", "cli.gs", "yfrog.com", "migre.me", "ff.im", "tiny.cc", "url4.eu",
+                         "twit.ac", "su.pr", "twurl.nl", "snipurl.com", "short.to", "BudURL.com", "ping.fm", "post.ly", "Just.as",
+                         "bkite.com", "snipr.com", "fic.kr", "loopt.us", "doiop.com", "short.ie", "kl.am", "wp.me",
+                         "rubyurl.com", "om.ly", "to.ly", "bit.do", "t.co", "lnkd.in", "db.tt", "qr.ae", "adf.ly", "bitly.com",
+                         "cur.lv", "ity.im", "q.gs", "po.st", "bc.vc", "twitthis.com", "u.to", "j.mp", "buzurl.com", "cutt.us",
+                         "u.bb", "yourls.org", "prettylinkpro.com", "scrnch.me", "filoops.info", "vzturl.com", "qr.net", "1url.com",
+                         "tweez.me", "v.gd", "tr.im", "link.zip.net", "replug.io", "rebrandly.com", "bl.ink", 'cutt.ly']
--- feature_extraction.py
+++ feature_extraction.py
@@ -4,10 +4,13 @@ import whois
 import re
 import time
 import string
+from const import constant
+
+# instance for constant class
+constitems = constant()
+
 
 # checking ip adress found or not in the url
-
-
 def having_ip_address(domain):
 
     if domain == "" or domain == None:
@@ -44,15 +47,8 @@ def shortening_service(domain):
         return -1
 
     else:
-
-        famous_short_urls = ["bit.ly", "tinyurl.com", "goo.gl", "rebrand.ly", "t.co", "youtu.be", "ow.ly", "w.wiki", "is.gd",
-                             "shorte.st", "go2l.ink", "x.co", "tr.im", "cli.gs", "yfrog.com", "migre.me", "ff.im", "tiny.cc", "url4.eu",
-                             "twit.ac", "su.pr", "twurl.nl", "snipurl.com", "short.to", "BudURL.com", "ping.fm", "post.ly", "Just.as",
-                             "bkite.com", "snipr.com", "fic.kr", "loopt.us", "doiop.com", "short.ie", "kl.am", "wp.me",
-                             "rubyurl.com", "om.ly", "to.ly", "bit.do", "t.co", "lnkd.in", "db.tt", "qr.ae", "adf.ly", "bitly.com",
-                             "cur.lv", "ity.im", "q.gs", "po.st", "bc.vc", "twitthis.com", "u.to", "j.mp", "buzurl.com", "cutt.us",
-                             "u.bb", "yourls.org", "prettylinkpro.com", "scrnch.me", "filoops.info", "vzturl.com", "qr.net", "1url.com",
-                             "tweez.me", "v.gd", "tr.im", "link.zip.net", "replug.io", "rebrandly.com", "bl.ink", 'cutt.ly']
+        # use of the instance
+        famous_short_urls = constitems.famous_short_urls
 
         status = 1
         if domain in famous_short_urls:
